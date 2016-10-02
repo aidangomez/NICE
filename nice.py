8610c76e336c009cfa46dbceeef452b2aeb9cb4b
Aidan Gomez <aidan.n.gomez@gmail.com>
2016-10-01 22:15:50 -0400

    Fix model errors

--- nice.py
+++ nice.py
@@ -15,7 +15,7 @@ class NICECombinationLayer:
 
     def __call__(self, input_1, input_2):
         output_1, output_2 = (input_1, input_2)
-        for (i, layer) in enumerate(layers):
+        for (i, layer) in enumerate(self.layers):
             if i % 2 == 0:
                 output_1, output_2 = layer(output_1, output_2)
             else:
@@ -27,13 +27,13 @@ class NICECombinationLayer:
 class NICELayer:
     def __init__(self,
                  coupling_layer,
-                 coupling_rule=additive_coupling):
-        self.couple = coupling_layer
-        self.coupling_f = coupling_function
+                 coupling_rule):
+        self.coupling_layer = coupling_layer
+        self.coupling_rule = coupling_rule
 
     def __call__(self, input_1, input_2):
         output_1 = input_1
-        output_2 = self.couple(input_2, self.coupling_f(input_1))
+        output_2 = self.coupling_rule(input_2, self.coupling_layer(input_1))
         return (output_1, output_2)
 
     def additive_coupling(a, b):
@@ -64,44 +64,48 @@ class CouplingLayer:
 
 
 class ReLUMLPLayer:
-    def __init__(self, input_size, output_size, name=uuid4()):
-        self.name = name
-        params[str(self.name)+"-W"] = theano.tensor.shared(0.01 * np.random.randn(input_size, output_size))
-        params[str(self.name)+"-b"] = theano.tensor.shared(np.zeros((output_size,)))
+    def __init__(self, input_size, output_size):
+        self.name = uuid4()
+        self.input_size = input_size
+        self.output_size = output_size
+        params[str(self.name)+"-W"] = theano.shared(0.01 * np.random.randn(input_size, output_size))
+        params[str(self.name)+"-b"] = theano.shared(np.zeros((output_size)))
 
     def __call__(self, inputs):
-        ip = theano.tensor.dot(params[str(self.name)+"-W"], inputs) + params[str(self.name)+"-b"]
+        ip = theano.tensor.dot(inputs, params[str(self.name)+"-W"]) + params[str(self.name)+"-b"]
         return theano.tensor.nnet.relu(ip)
 
 
 def log_loss(output, s):
-    component_loss = -(np.log(1 + exp(output)) + np.log(1 + exp(-output)))
-    return component_loss.sum() +  np.sum(s))
+    component_loss = -(np.log(1 + np.exp(output)) + np.log(1 + np.exp(-output)))
+    return component_loss.sum() +  s.sum()
 
 def build_model():
-    x_1 = tensor.vector('x', dtype='int8')
-    x_2 = tensor.vector('x', dtype='int8')
-    y_1 = tensor.vector('y', dtype='int8')
-    y_2 = tensor.vector('y', dtype='int8')
+    x_1 = theano.tensor.matrix('x_1', dtype=theano.config.floatX)
+    x_2 = theano.tensor.matrix('x_2', dtype=theano.config.floatX)
 
-    params["s"] = theano.tensor.shared(np.zeros((y_1.shape[0] + y_2.shape[0])))
+    x_1_size = 392
+    x_2_size = 392
+    s = np.zeros(x_1_size + x_2_size, dtype=theano.config.floatX)
+    params["s"] = theano.shared(s, name="s")
 
-    coupling_1 = CouplingLayer(x_1.shape[0], 5, 1000, x_2.shape[0])
-    nice_1 = NICELayer(coupling_1)
-    coupling_2 = CouplingLayer(x_2.shape[0], 5, 1000, x_1.shape[0])
-    nice_2 = NICELayer(coupling_2)
-    coupling_3 = CouplingLayer(x_1.shape[0], 5, 1000, x_2.shape[0])
-    nice_3 = NICELayer(coupling_3)
-    coupling_4 = CouplingLayer(x_2.shape[0], 5, 1000, x_1.shape[0])
-    nice_4 = NICELayer(coupling_4)
+    coupling_1 = CouplingLayer(x_1_size, 5, [1000]*5, x_2_size)
+    nice_1 = NICELayer(coupling_1, NICELayer.additive_coupling)
+    coupling_2 = CouplingLayer(x_2_size, 5, [1000]*5, x_1_size)
+    nice_2 = NICELayer(coupling_2, NICELayer.additive_coupling)
+    coupling_3 = CouplingLayer(x_1_size, 5, [1000]*5, x_2_size)
+    nice_3 = NICELayer(coupling_3, NICELayer.additive_coupling)
+    coupling_4 = CouplingLayer(x_2_size, 5, [1000]*5, x_1_size)
+    nice_4 = NICELayer(coupling_4, NICELayer.additive_coupling)
 
     nice_stack = NICECombinationLayer([nice_1, nice_2, nice_3, nice_4])
 
     nice_output_1, nice_output_2 = nice_stack(x_1, x_2)
-    pred = np.concatenate([nice_output_1, nice_output_2], axis=1) * np.exp(params["s"])
+    output = theano.tensor.concatenate([nice_output_1, nice_output_2], axis=0)
+    pred = output * np.exp(params["s"])
 
     f_pred = theano.function([x_1, x_2], pred)
 
     cost = log_loss(output, params["s"])
 
-    return x_1, x_2, y_1, y_2, f_pred_prob, f_pred, cost
+    return params, x_1, x_2, f_pred, cost
